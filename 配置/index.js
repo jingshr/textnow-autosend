e0b00dcbb1f5295d9e4c4d643dfcbb1d08e250e9
jingshr <91071619+jingshr@users.noreply.github.com>
2021-09-27 12:43:46 +0800

    Update and rename config/index.js to 配置/index.js

--- 配置/index.js
+++ 配置/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   username: process.env.TEXTNOW_USERNAME || "",
   password: process.env.TEXTNOW_PASSWORD || "",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
+  recipient: process.env.TEXTNOW_RECIPIENT || "(516) 855-5259",
   message: process.env.TEXTNOW_MESSAGE || "autosend message",
 };
